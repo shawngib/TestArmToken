41bc1bd3e22fc1849c217bc72d40e0500b204665
Shawn Gibbs <shawngib@microsoft.com>
2020-07-08 16:42:45 -0400

    Update Get-ArmToken.ps1

--- Get-ArmToken.ps1
+++ Get-ArmToken.ps1
@@ -1,4 +1,12 @@
-param([string] $name)
+param(
+  $workspaceName = "Central-LAWS-sharedsvcs-log",
+  $ruleName = "Log Analytics ($workspaceName)"
+)
+
+# Find the ResourceId for the Log Analytics workspace
+
+$workspaceResource = Get-AzResource -ResourceType "Microsoft.OperationalInsights/workspaces" -Name $workspaceName
+$workspaceId = $workspaceResource.ResourceId
 function Get-AzCachedAccessToken()
 {
     $ErrorActionPreference = 'Stop'
@@ -16,4 +24,48 @@ function Get-AzCachedAccessToken()
     $token.AccessToken
 }
 $token = Get-AzCachedAccessToken
-Write-Output $token
+
+#setup diag settings
+$uri = "https://management.azure.com/providers/microsoft.aadiam/diagnosticSettings/{0}?api-version=2017-04-01-preview" -f $ruleName
+$body = @"
+{
+    "id": "providers/microsoft.aadiam/diagnosticSettings/$ruleName",
+    "type": null,
+    "name": "Log Analytics",
+    "location": null,
+    "kind": null,
+    "tags": null,
+    "properties": {
+      "storageAccountId": null,
+      "serviceBusRuleId": null,
+      "workspaceId": "$workspaceId",
+      "eventHubAuthorizationRuleId": null,
+      "eventHubName": null,
+      "metrics": [],
+      "logs": [
+        {
+          "category": "AuditLogs",
+          "enabled": true,
+          "retentionPolicy": { "enabled": false, "days": 0 }
+        },
+        {
+          "category": "SignInLogs",
+          "enabled": true,
+          "retentionPolicy": { "enabled": false, "days": 0 }
+        }
+      ]
+    },
+    "identity": null
+  }
+"@
+
+$headers = @{
+    "Authorization" = "Bearer $token"
+    "Content-Type"  = "application/json"
+}
+$response = Invoke-WebRequest -Method Put -Uri $uri -Body $body -Headers $headers
+
+if ($response.StatusCode -ne 200) {
+    throw "an error occured: $($response | out-string)"
+}
+write-output $response
